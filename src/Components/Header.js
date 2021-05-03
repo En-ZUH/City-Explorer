1698120705add19a3bac4947ba28017af61b6572
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-04 02:31:40 +0300

    done

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div class='header'>
+        <h1>City Explorer</h1><h5>Enter a location below to learn about the weather, resturants, movies, and more!</h5>
+
+      </div>
+    );
+  }
+}
---
+++ src/Components/Img.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+import '../index.css';
+import Form from 'react-bootstrap/Form';
+
+export default class Img extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <p class='p1'>{this.props.display_name}</p>
+
+          <p>{this.props.display_name}  {this.props.lat}  {this.props.lon}</p>
+        </Form>
+
+        <Image src={this.props.imageSrc1} fluid />
+
+
+      </div>
+    );
+  }
+}
